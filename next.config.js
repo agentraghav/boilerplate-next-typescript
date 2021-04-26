3239f0d109da39891dc0417362dfc2d8158e50c1
agentraghav <raghvendrarathore2@gmail.com>
2021-04-26 17:47:57 +0530

    minor change

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const { i18n } = require('./next-i18next.config');
 const withImages = require('next-images');
 module.exports = withImages({
+  fileExtensions: ['jpg', 'jpeg', 'png', 'gif'],
   i18n,
 });
