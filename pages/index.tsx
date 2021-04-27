8670333799f1f5850d1fee377c36aa9a193a49c8
agentraghav <raghvendrarathore2@gmail.com>
2021-04-27 14:10:40 +0530

    minor change

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,12 +14,7 @@ const HomePage: React.FunctionComponent<any> = () => {
         href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap'
         rel='stylesheet'
       />
-      <link
-        href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css'
-        rel='stylesheet'
-        crossOrigin="anonymous"
-        integrity='sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6'
-      />
+ 
       <Header text_btn1={t('btn-text1')} text_btn2={t('btn-text2')} />
       <div className={styles.container}>
         <PageLayoutWithSEO
