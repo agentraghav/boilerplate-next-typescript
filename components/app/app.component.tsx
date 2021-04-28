861a8bb0b0080752f98ca38c155e7895d4e8bfbf
agentraghav <raghvendrarathore2@gmail.com>
2021-04-28 11:25:06 +0530

    change

--- components/app/app.component.tsx
+++ components/app/app.component.tsx
@@ -5,31 +5,7 @@ const AppComponent: React.FC = () => {
   const { t } = useTranslation('common');
   return (
     <React.Fragment>
-      <div className='bg-class'>
-        <div className='head'>
-          <p className='heading'>{t('title')}</p>
-          <p className='sub-head'>
-            {t('about_para')}{' '}
-            <span>
-              <a
-                style={{ color: '#7D7D7D' }}
-                href='https://github.com/jalantechnologies/boilerplate-next-typescript#readme'>
-                {t('readme')}
-              </a>
-            </span>{' '}
-          </p>
-        </div>
-        <div className='next-logo'></div>
-        <div className='base-logo'></div>
-        <div className='eslint-logo'></div>
-        <div className='storybook-logo'></div>
-        <div className='seo-logo'></div>
-        <div className='ts-logo'></div>
-        <div className='sentry-logo'></div>
-        <div className='i18n-logo'></div>
-        <div className='jest-logo'></div>
-        <div className='formik-logo'></div>
-      </div>
+      <div className='next-logo'></div>
     </React.Fragment>
   );
 };
